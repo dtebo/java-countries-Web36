b66d79242f0e484d543815efc86e42eb8045b256
Darren Tebo <darrentebo83@outlook.com>
2020-12-03 16:02:10 -0500

    added the list all and starts with endpoints

--- javacountries/src/main/java/com/lambdaschool/javacountries/controllers/CountryController.java
+++ javacountries/src/main/java/com/lambdaschool/javacountries/controllers/CountryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -16,16 +17,38 @@ public class CountryController {
     @Autowired
     CountryRepository countryRepository;
 
+    private List<Country> findCountries(List<Country> myCountries, CheckCountry tester){
+        List<Country> tempList = new ArrayList<>();
+
+        for(Country c : myCountries){
+            if(tester.test(c)){
+                tempList.add(c);
+            }
+        }
+
+        return tempList;
+    }
+
     // http://localhost:2019/names/all
     @GetMapping(value = "/names/all", produces = "application/json")
     public ResponseEntity<?> getAllCountries(){
         List<Country> myCountries = new ArrayList<>();
         countryRepository.findAll().iterator().forEachRemaining(myCountries::add);
+        myCountries.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
 
         return new ResponseEntity<>(myCountries, HttpStatus.OK);
     }
 
     // http://localhost:2019/names/start/u
+    @GetMapping(value = "/names/start/{letter}", produces = "application/json")
+    public ResponseEntity<?> listCountriesStartingWithLetter(@PathVariable char letter){
+        List<Country> myCountries = new ArrayList<>();
+        countryRepository.findAll().iterator().forEachRemaining(myCountries::add);
+
+        List<Country> rtnList = findCountries(myCountries, c -> c.getName().charAt(0) == letter);
+
+        return new ResponseEntity<>(rtnList, HttpStatus.OK);
+    }
 
     // http://localhost:2019/population/total
 
